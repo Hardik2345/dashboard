ae0b8f1dd5af7cdc2ee19fba1c90c2d3e4efbe5f
Hardik Parikh <hardik@Hardiks-MacBook-Air-2.local>
2025-11-13 18:31:05 +0530

    fixing liniting errors

--- scripts/producer-firehose.js
+++ scripts/producer-firehose.js
@@ -1,8 +1,8 @@
 // scripts/producer-firehose.js
 // MongoDB Change Streams → Kinesis Data Firehose (DirectPut) → S3 (GZIP NDJSON)
 
-import { MongoClient } from 'mongodb';
-import { FirehoseClient, PutRecordBatchCommand } from '@aws-sdk/client-firehose';
+const { MongoClient } = require('mongodb');
+const { FirehoseClient, PutRecordBatchCommand } = require('@aws-sdk/client-firehose');
 
 const {
   SRC_MONGO_URI,
