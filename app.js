4fcc55151cd47f5f8d16187e803621b8e2b21208
Hardik Parikh <hardik@Hardiks-MacBook-Air-2.local>
2025-11-22 18:35:44 +0530

    removing forced assignment of node env

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 // Force production mode by default when running the server from this codebase.
-process.env.NODE_ENV = 'development';
 const express = require("express");
 const cors = require("cors");
 const session = require('express-session');
