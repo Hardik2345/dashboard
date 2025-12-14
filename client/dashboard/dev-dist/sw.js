da9f7d832348bdf8db98a18a6764c11e6e811d3b
Aryan Arora <artistaryan18@gmail.com>
2025-12-14 12:22:43 +0530

    fix: update revision for index.html in service worker

--- client/dashboard/dev-dist/sw.js
+++ client/dashboard/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-38bb0eb2'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.2c584sv8cec"
+    "revision": "0.vonvssmnh08"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
