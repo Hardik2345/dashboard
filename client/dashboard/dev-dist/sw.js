83c8a5df17e797b9a3a0324bd65fd2b9d5120474
Hardik Parikh <hardik@Hardiks-MacBook-Air-2.local>
2025-12-14 12:12:47 +0530

    fixing connection pool issues

--- client/dashboard/dev-dist/sw.js
+++ client/dashboard/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.7gkkonu477c"
+    "revision": "0.3i4t04ii6kg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
