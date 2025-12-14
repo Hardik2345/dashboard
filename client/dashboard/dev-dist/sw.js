abc01f59211c6bd0b8db517945f58647f2d5c5fb
Aryan Arora <artistaryan18@gmail.com>
2025-12-14 09:34:16 +0530

    Refactor code structure for improved readability and maintainability

--- client/dashboard/dev-dist/sw.js
+++ client/dashboard/dev-dist/sw.js
@@ -67,7 +67,7 @@ if (!self.define) {
     });
   };
 }
-define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
+define(['./workbox-38bb0eb2'], (function (workbox) { 'use strict';
 
   self.skipWaiting();
   workbox.clientsClaim();
@@ -82,11 +82,13 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.7gkkonu477c"
+    "revision": "0.ttb1t4ekem"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
-    allowlist: [/^\/$/]
+    allowlist: [/^\/$/],
+    denylist: [/^\/api/]
   }));
+  workbox.registerRoute(/^\/api/, new workbox.NetworkOnly(), 'GET');
 
 }));
