e63a1bd41fa24de61d1f05f2ea4c56331402c791
Aryan Arora <artistaryan18@gmail.com>
2026-02-26 14:40:53 +0530

    push notification implemented v1

--- client/dashboard/public/firebase-messaging-sw.js
+++ client/dashboard/public/firebase-messaging-sw.js
@@ -1,27 +1,20 @@
-// Give the service worker access to Firebase Messaging.
-// Note: You must allow this file to be loaded from the root logic.
-
 importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js');
 
-
-// Initialize the Firebase app in the service worker by passing in
-// your app's Firebase config object.
-// https://firebase.google.com/docs/web/setup#config-object
 firebase.initializeApp({
-  apiKey: "AIzaSyAfDggFGLHrR91uUWpxICSYJu57XkTDSWg",
-  authDomain: "datum-push-test.firebaseapp.com",
-  projectId: "datum-push-test",
-  storageBucket: "datum-push-test.firebasestorage.app",
-  messagingSenderId: "404123337738",
-  appId: "1:404123337738:web:eaf5899b153e9e5a928bf3"
+  apiKey: 'AIzaSyCY6hO0f5Y-vukki1C3hbFkFNun-wRJGgM',
+  authDomain: 'dashboard-notifications-fde8d.firebaseapp.com',
+  projectId: 'dashboard-notifications-fde8d',
+  storageBucket: 'dashboard-notifications-fde8d.firebasestorage.app',
+  messagingSenderId: '159691205555',
+  appId: '1:159691205555:web:f5a1951d505ed8395679c6',
 });
 
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage((payload) => {
   console.log('[firebase-messaging-sw.js] Received background message ', payload);
-  // Customize notification here
+
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
