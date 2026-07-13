435763c16fed354bdfc87b52ce27ace3ee9a7f13
Aryan Arora <artistaryan18@gmail.com>
2026-07-13 13:57:08 +0530

    added more event enums in session model

--- alerts-service/models/session.js
+++ alerts-service/models/session.js
@@ -1,12 +1,23 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const sessionSchema = new mongoose.Schema({
+const sessionSchema = new mongoose.Schema(
+  {
     event_id: { type: String, required: true },
     idempotency_key: { type: String, unique: true, required: true },
     event_type: {
-        type: String,
-        enum: ['add_to_cart', 'checkout_initiated', 'buy_now', 'page_viewed', 'add_to_cart_rs'],
-        required: true
+      type: String,
+      enum: [
+        "add_to_cart",
+        "checkout_initiated",
+        "buy_now",
+        "page_viewed",
+        "add_to_cart_rs",
+        "video_open",
+        "video_add_to_cart",
+        "video_more_info",
+        "video_close",
+      ],
+      required: true,
     },
     session_id: { type: String, required: true },
     variantId: { type: String },
@@ -14,12 +25,12 @@ const sessionSchema = new mongoose.Schema({
     cart_token: { type: String },
     checkout_token: { type: String },
     user_agent: { type: String },
-    url: { type: String }
-}, {
+    url: { type: String },
+  },
+  {
     timestamps: true,
-    collection: 'sessions'
-});
+    collection: "sessions",
+  },
+);
 
-module.exports = mongoose.model('Session', sessionSchema);
-
-
+module.exports = mongoose.model("Session", sessionSchema);
